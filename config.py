f12596fe2b2f44656aff42000a5fee40bd58ac38
Vhal999Vhal999 <107368993+Vhal999Vhal999@users.noreply.github.com>
2024-08-15 09:08:55 +0530

    Update config.py
    Signed-off-by: Vhal999Vhal999 <107368993+Vhal999Vhal999@users.noreply.github.com>

--- config.py
+++ config.py
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/25efe6aa029c6baea73ea.jpg"
+    "START_IMG_URL", "https://graph.org/file/c7abe7799ccd4fed33a0f.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/ba1c1ee7cb1349e5e507f.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
