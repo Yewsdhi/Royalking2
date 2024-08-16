905ac3352faf371f2d342f6380e5f6ab8b1cfadf
The-stone <147587252+the-stonex@users.noreply.github.com>
2024-08-15 23:05:44 -0700

    Update welcome.py
    Signed-off-by: The-stone <147587252+the-stonex@users.noreply.github.com>

--- AnonXMusic/plugins/tools/welcome.py
+++ AnonXMusic/plugins/tools/welcome.py
@@ -16,7 +16,6 @@ from asyncio import sleep
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from logging import getLogger
-from AnonXMusic.utils.Anon_ban import admin_filter
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
